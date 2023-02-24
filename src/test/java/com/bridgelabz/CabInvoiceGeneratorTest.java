e3823a8b6d5d6d2d4177cec044a36443e762e026
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-02-24 22:03:58 +0530

    Added : calculate fare

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -1,6 +1,9 @@
 package com.bridgelabz;
 
 public class CabInvoiceGeneratorTest {
-
+    public void calculateFareOfOneRide() {
+        CabInvoiceGenerator obj = new CabInvoiceGenerator();
+        double test = obj.calculateFare(10, 15);
+        Assert.assertEquals(115.0, test, 0.0);
     }
 }
